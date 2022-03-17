143c2a60378087408ebe054c7ff094bb06720ce1
Carlos Fernando Torres <cftorres20@gmail.com>
2022-03-17 20:34:02 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let entrada = parseInt(prompt("Ingresa un numero del 1 al 5"));
+
+while (entrada != 3) {
+  switch (entrada) {
+    case 1:
+      break;
+
+    case 2:
+      break;
+
+    case 4:
+      break;
+
+    case 5:
+      break;
+
+    default:
+      alert("Ingrese un numero dentro del rango 1 a 5 ");
+      break;
+  }
+
+  entrada = parseInt(prompt("Ingresar otro dato"));
+}
